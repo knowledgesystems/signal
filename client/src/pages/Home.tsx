f851b44897f2867e068952b279a7f0adc0c3dc73
Ino de Bruijn <ino@ino.pm>
2019-10-01 18:27:48 -0400

    minor change blurb 🔧

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -14,8 +14,8 @@ class Home extends React.Component<IHomeProps>
         return (
             <div>
                 <div style={{paddingLeft:8, maxWidth: 1500}}>The
-                INSIGHT (<b>IN</b>tegration of <b>S</b>omatic with <b>G</b>ermline <b>H</b>eritability
-                in <b>T</b>umorigenesis) resource integrates germline and
+                INSIGHT (<b>in</b>tegration of <b>s</b>omat<b>i</b>c with <b>g</b>ermline <b>h</b>eritability
+                in <b>t</b>umorigenesis) resource integrates germline and
                 somatic alterations identified by clinical sequencing of
                 active cancer patients. Provided here are pathogenic germline
                 variants and their tumor-specific zygosity changes by gene,

11e7cba26e40e17bb06d6a7e252cc807ab649600
Onur Sumer <s.onur.sumer@gmail.com>
2020-09-29 16:22:55 -0400

    move blurb back to the home page
    Signed-off-by: Onur Sumer <s.onur.sumer@gmail.com>

--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -43,23 +43,10 @@ class Explore extends React.Component<IExploreProps>
         }
     }
 
-    private get blurb() {
-        return (
-            <div style={{paddingLeft:8, maxWidth: 1500}}>
-                The SIGNAL (<u>S</u>omatic <u>I</u>ntegration of <u>G</u>ermli<u>n</u>e <u>Al</u>terations in cancer)
-                resource integrates germline and somatic alterations identified by clinical sequencing of
-                active cancer patients. Provided here are pathogenic germline variants and their tumor-specific
-                zygosity changes by gene, lineage, and cancer type in 17,152 prospectively sequenced cancer patients.
-            </div>
-        );
-    }
-
     public render()
     {
         return (
             <div>
-                {this.blurb}
-                <hr />
                 {!this.isLoading() &&
                     <Row className="mb-2">
                         <PenetranceFilterPanel
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,14 +14,34 @@ interface IHomeProps {
 
 class Home extends React.Component<IHomeProps>
 {
+    // TODO get these numbers from the store if possible
+
+    private get numberOfPatients() {
+        return (17152).toLocaleString('en-US');
+    }
+
+    private get numberOfCancerTypes() {
+        return (55).toLocaleString('en-US');
+    }
+
+    private get blurb() {
+        return (
+            <div style={{fontSize: "85%"}}>
+                The SIGNAL resource integrates germline and somatic alterations identified by clinical sequencing of
+                active cancer patients. Provided here are pathogenic germline variants and their tumor-specific
+                zygosity changes by gene, lineage, and cancer type in {this.numberOfPatients} prospectively sequenced cancer patients.
+            </div>
+        );
+    }
+
     public render()
     {
         return (
             <div>
-                <Row className="mb-5">
+                <Row className="mb-3">
                     <Col
                         md={6}
-                        className={'mx-auto d-flex flex-column align-items-center '}
+                        className="mx-auto d-flex flex-column align-items-center"
                     >
                         <span className="home-page-logo">
                             <i className="fa fa-arrow-up" style={{color: "#FF9900"}} /> Signal<span className="home-page-logo-db">DB</span>
@@ -29,11 +49,21 @@ class Home extends React.Component<IHomeProps>
                         <span className="home-page-logo-title">
                             <strong><u>S</u></strong>omatic <strong><u>I</u></strong>ntegration of <u><strong>G</strong></u>ermli<u><strong>n</strong></u>e <u><strong>Al</strong></u>terations in Cancer
                         </span>
-                        <span className="home-page-logo-title-info">17,152 patients and 55 cancer types</span>
+                        <span className="home-page-logo-title-info">{this.numberOfPatients} patients and {this.numberOfCancerTypes} cancer types</span>
                     </Col>
                 </Row>
                 <Row className="mb-5">
                     <PenetranceFilterPanel geneFrequencyStore={this.props.frequencyStore} />
+                </Row>
+                <Row className="mb-5">
+                    <Col md={10} className="mx-auto d-flex align-items-center">
+                        <input className="m-auto" type="text" placeholder="Dummy Search..."/>
+                    </Col>
+                </Row>
+                <Row className="mb-5">
+                    <Col md={10} className="mx-auto d-flex text-center">
+                        {this.blurb}
+                    </Col>
                 </Row>
             </div>
         );

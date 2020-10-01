e41de3c203d7f60f625c4b1f14b683eca6c82364
Xiang Li <xlvv9@mail.missouri.edu>
2020-10-01 15:14:23 -0400

    update examples with signal mutations

--- src/util/Constants.ts
+++ src/util/Constants.ts
@@ -1,18 +1,16 @@
-// This component is from Genome Nexus Frontend originally
-
 // TODO we could make some tweaks to make it contains all three types we support (gene, region, variant)
 export const EXAMPLE_DATA_GRCH37 = [
     {
-        value: '17:g.37880220T>C',
-        label: '17:g.37880220T>C (ERBB2 L755S)',
+        value: '13:g.32921033G>C',
+        label: '13:g.32921033G>C (BRCA2 R2336P)',
     },
-    { value: '7:g.55249071C>T', label: '7:g.55249071C>T (EGFR T790M)' },
+    { value: '13:g.32972901_32972904del', label: '13:g.32972901_32972904del (BRCA2 I3418Kfs*8)' },
     {
-        value: '7:g.55242468_55242481delinsAC',
-        label: '7:g.55242468_55242481delinsAC (EGFR L747_T751delinsP)',
+        value: '17:g.41243783_41243784insT',
+        label: '7:g.41243783_41243784insT (BRCA1 N1255Kfs*12)',
     },
     {
-        value: '7:g.55249017_55249018insCCA',
-        label: '7:g.55249017_55249018insCCA (EGFR H773dup)',
+        value: '17:g.7577548C>T',
+        label: '17:g.7577548C>T (TP53 G245S)',
     },
 ];
